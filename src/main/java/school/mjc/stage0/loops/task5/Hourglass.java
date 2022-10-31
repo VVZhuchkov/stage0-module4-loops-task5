4848ca12b487c0ff79a39ac67410abd7e85fe9e2
VVZhuchkov <vzhuchkov@tut.by>
2022-10-31 20:59:00 +0300

    3

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -4,7 +4,7 @@ public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < height; j++) {
-                if (j < i || j > height - i - 1) {
+                if ((j < i || j > height - i - 1) && (j > i || j < height - i - 1)) {
                     System.out.print(" ");
                 } else {
                     System.out.print("8");
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
-
+    public void printRectangleFrom8s(int length, int height) {
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < length; i++) {
+                if (j == 0 || j == height - 1) {
+                    System.out.print("8");
+                } else if (i == 0 || i == length - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
